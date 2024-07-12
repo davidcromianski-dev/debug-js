c856944efdb2c8475d57290b601a1e1363158069
David Cromianski <david.cromianski@ixcsoft.com.br>
2024-07-12 12:23:17 -0300

    -

--- node/server.js
+++ node/server.js
@@ -7,12 +7,14 @@ app.use(express.json());
 
 // A simple GET endpoint
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+  const { num1, num2 } = req.query;
 
-// A simple POST endpoint
-app.post("/echo", (req, res) => {
-  res.json({ message: "Received", data: req.body });
+  if (!num1 || !num2) {
+    return res.send("Please provide both num1 and num2");
+  }
+  const sum = num1 + num2;
+
+  res.send(`The sum of ${num1} and ${num2} is ${sum}`);
 });
 
 // Start the server
